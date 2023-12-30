026d5207bd496f3a32bc81537d954b1bfd9239b8
Bruno Alpachino <brunolalpachino@gmail.com>
2023-12-30 09:32:56 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import Offers from "./components/Offers.jsx";
-import Plan from "./components/Plan.jsx";
-import Rooms from "./components/Rooms.jsx";
-import ImageSlider from "./components/ImageSlider.jsx";
-import Footer from "./components/Footer.jsx";
-import Testimonials from "./components/Testimonials.jsx";
-import Services from "./components/Services.jsx";
+
 import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import MoreServices from "./components/MoreServices.jsx";
 import HomePage from "./pages/HomePage.jsx";
