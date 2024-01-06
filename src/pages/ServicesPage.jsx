0ff80e9f4c1cfaf7618cf7d1c98c112694345957
Bruno Alpachino <brunolalpachino@gmail.com>
2024-01-06 09:19:01 +0300

    made changes to the service page

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -6,7 +6,7 @@ const ServicesPage = () => {
     "https://images.pexels.com/photos/7551627/pexels-photo-7551627.jpeg?auto=compress&cs=tinysrgb&w=1200";
 
   return (
-    <div className="h-screen ">
+    <div className="h-screen">
       <div className="relative h-[300px] w-full ">
         <img
           className="top-0 left-0 w-full h-full object-cover "
@@ -26,9 +26,29 @@ const ServicesPage = () => {
             before:z-[-5] absolute top-0 left-0 w-full h-full opacity-40"
         />
         <div className=" absolute top-0 left-0 w-full h-full  flex items-center justify-center text-white">
-          <h4>we are her</h4>
+          <h4 className="font-bold text-4xl">Nurses (RN’s and RPN’s)</h4>
         </div>
       </div>
+      <div className="p-2 mt-4 grid md:grid-cols-2 ">
+        <div className="mb-4">
+          <h4 className="font-bold text-4xl">What we offer</h4>
+          <p className="text-base mt-4">
+            Our registered and licensed nurses are dedicated to providing
+            comprehensive care that exceeds expectations, whether it's within
+            the hospital's clinical setting or the familiar comfort of your
+            home. Their expertise extends across various domains, ensuring a
+            holistic approach to your health and well-being.
+          </p>
+        </div>
+        <div className="sm:mt-2">
+          <img
+            className="object-cover w-full h-full"
+            src="https://images.pexels.com/photos/7551677/pexels-photo-7551677.jpeg?auto=compress&cs=tinysrgb&w=1200"
+            alt=""
+          />
+        </div>
+      </div>
+      <h4 className="text-center font-bold text-3xl mt-4">Services</h4>
     </div>
   );
 };
